0e72607e216915de9913760718e9465259ea16eb
Alterace <nielsroestlr@gmail.com>
2021-01-19 13:38:53 +0100

    Push

---
+++ Santa's secret coke addiction/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform followTarget;
+    private float offsetDistance = 2.5f;
+    private float currentX = 0f;
+    private float currentY = 0f;
+    private float mouseSensitivity;
+    private Vector3 offset;
+    private float yAngleMin = -89.9f;
+    private float yAngleMax = 89.9f;
+    private Vector3 desiredPosition;
+    private float hitOffset;
+    private void Awake()
+    {
+        followTarget = GameObject.FindGameObjectWithTag("Player").transform;
+        mouseSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 1f);
+        offset = new Vector3(0, 0, offsetDistance);
+    }
+    private void Update()
+    {
+
+        currentX += Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity * 600;
+        if (currentX < -360)
+            currentX += 360;
+        else if (currentX > 360)
+            currentX -= 360;
+
+        currentY += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity * 600;
+        currentY = Mathf.Clamp(currentY, yAngleMin, yAngleMax);
+    }
+    private void LateUpdate()
+    {
+        offsetDistance = 2.5f;
+        MoveCamera();
+        transform.LookAt(followTarget);
+        DebugMovement();
+    }
+
+    void DebugMovement()
+    {
+        if(offsetDistance < 0.1f)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = false;
+            this.enabled = false;
+        }
+    }
+    void MoveCamera()
+    {
+        offset = new Vector3(0, 0, offsetDistance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        desiredPosition = followTarget.position + rotation * offset;
+        CheckCamera(desiredPosition);
+        offset = new Vector3(0, 0, offsetDistance);
+        rotation = Quaternion.Euler(currentY, currentX, 0);
+        desiredPosition = followTarget.position + rotation * offset;
+        transform.position = desiredPosition;
+    }
+
+    void CheckCamera(Vector3 wantedPosition)
+    {
+        RaycastHit hit;
+        Vector3 direction = wantedPosition - followTarget.position;
+        if (Physics.Raycast(followTarget.position, direction, out hit, maxDistance: 2.5f) && hit.collider.tag != "Player")
+        {
+            offsetDistance = hit.distance - 0.1f;      
+        }
+        else
+        {
+            offsetDistance = 2.5f;
+        }
+    }
+}
---
+++ Santa's secret coke addiction/Assets/Scripts/Movement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController cc;
+    private Transform cameraTransform;
+    private Vector3 movePosition;
+    private float movementSpeed = 10f;
+    private Vector3 finalPosition;
+    private Vector3 direction;
+
+    private const float gravitationalConstant = 9.81f;
+    private float timeInAir = 0f;
+    private float jumpHeight = 400f;
+    private Vector3 jumpForce = new Vector3(0, 0, 0);
+    private Vector3 verticalForce;
+
+    private void Awake()
+    {
+        timeInAir = 0f;
+        cc = gameObject.GetComponent<CharacterController>();
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+    private void Update()
+    {
+        movePosition = new Vector3(0, 0, 0);
+        if(Input.GetKey("w"))
+        {
+            movePosition += Vector3.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            movePosition += Vector3.back;
+        }
+        if (Input.GetKey("d"))
+        {
+            movePosition += Vector3.right;
+        }
+        if (Input.GetKey("a"))
+        {
+            movePosition += Vector3.left;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed *= 2;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movementSpeed /= 2;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+
+        ApplyVerticalForce();
+        CalcFinalPosition();
+        MoveCharacter();
+    }
+
+
+    void Jump()
+    {
+        cc.Move(new Vector3(0, -0.01f, 0)) ;
+        if(cc.isGrounded)
+        {
+            Debug.Log("Jumping");
+            jumpForce.y = jumpHeight;
+        }
+    }
+
+    void ApplyVerticalForce()
+    {
+        if (cc.isGrounded)
+            timeInAir = 0;
+        else
+         timeInAir += Time.deltaTime;
+
+        verticalForce = new Vector3(0, 0, 0);
+        verticalForce.y = (-gravitationalConstant * timeInAir) + (jumpForce.z * timeInAir);
+        cc.Move(verticalForce);
+
+    }
+
+    void CalcFinalPosition()
+    {
+        Quaternion cameraMoveRotation = Quaternion.Euler(0, cameraTransform.rotation.eulerAngles.y, 0);
+        finalPosition = cameraMoveRotation * movePosition;
+    }
+    void RotateCharacter()
+    {
+        if (movePosition != Vector3.zero)
+        {
+            direction = (transform.position + finalPosition) - (transform.position);
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        RotateCharacter();
+        cc.Move(finalPosition * Time.deltaTime * movementSpeed);
+    }
+
+    
+}
